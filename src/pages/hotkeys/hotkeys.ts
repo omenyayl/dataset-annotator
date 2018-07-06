d2d569c350697515a3b2edb3b6731dfee8088448
Robert Hitt <rhitt@gmu.edu>
2018-07-06 14:00:28 -0400

    Made the keybinding config inputs a lot better

--- src/pages/hotkeys/hotkeys.ts
+++ src/pages/hotkeys/hotkeys.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HotkeyProvider } from '../../providers/hotkeys/hotkeys';
 import { NavProxyService } from '../../providers/nav-proxy/nav-proxy';
@@ -12,6 +12,7 @@ import { NavProxyService } from '../../providers/nav-proxy/nav-proxy';
 export class HotkeysPage {
 
   private hotkeys: FormGroup;
+  private inputs: string[];
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -26,10 +27,43 @@ export class HotkeysPage {
       rectangle: [this.hotkeyProvider.hotkeys.rectangle],
       polygon: [this.hotkeyProvider.hotkeys.polygon]
     })
+
+    this.inputs = [];
   }
 
   updateHotkeys() {
     this.hotkeyProvider.hotkeys = this.hotkeys.value;
     this.navProxy.popMaster(HotkeysPage);
+  }
+
+  markInput($event) {
+    // Only mark inputs that are not Shift and are not currently marked.
+    console.log($event);
+    if($event.key !== "Shift" &&
+       this.inputs !== undefined && this.inputs.indexOf($event.key) == -1) {
+
+      if($event.key !== "Control" && $event.ctrlKey == true
+         && this.inputs.indexOf("Control") == -1) {
+        this.inputs.push("Control");
+      }
+
+      if($event.key !== "Alt" && $event.altKey == true
+         && this.inputs.indexOf("Alt") == -1) {
+        this.inputs.push("Alt");
+      }
+
+      this.inputs.push($event.key);
+    }
+  }
+
+  saveCompositeInput($event, name) {
+    if(this.inputs !== undefined && this.inputs.length !== 0) {
+      const fc = this.hotkeys.controls[name] as FormControl;
+      fc.setValue(this.inputs.join(" + "));
+
+      this.inputs = [];
+
+      $event.target.select();
+    }
   }
 }
