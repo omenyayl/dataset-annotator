f90dc554cff647119231c78245545063496006bd
Oleg Menyaylenko <omenyayl@masonlive.gmu.edu>
2018-07-06 14:56:40 -0400

    no more initlines and initrects

--- src/drawers/rectangle-drawer.ts
+++ src/drawers/rectangle-drawer.ts
@@ -19,7 +19,6 @@ export class RectangleDrawer extends Drawer{
     constructor(context: CanvasRenderingContext2D,
                 imageProvider: ImageProvider) {
         super(context, imageProvider);
-        this.initBoxes();
         this.boxes = this.getBoxes();
     }
 
@@ -77,16 +76,6 @@ export class RectangleDrawer extends Drawer{
         }
     }
 
-    initBoxes() {
-        let currentImage = super.getImageProvider().currentImage;
-
-        if (currentImage && ! super.getImageProvider().annotations.hasOwnProperty(currentImage.src)){
-            super.getImageProvider().annotations[currentImage.src] = {
-                boxes: []
-            }
-        }
-
-    }
 
     drawCircle(coordinates: CoordinatesObject, color = DEFAULT_COLOR){
         super.getContext().beginPath();
