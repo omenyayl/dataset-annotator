6bc3833aab724f7731d9f267ae7b25b1d6e37910
Unknown <omenyayl@masonlive.gmu.edu>
2018-07-09 12:25:21 -0400

    Auto stash before merge of "master" and "origin/feature-refactor"

---
+++ src/providers/annotations/annotations.ts
@@ -0,0 +1,76 @@
+import {Injectable, NgModule} from '@angular/core';
+import {AnnotationObject} from "../../objects/annotation-object";
+import { ImageProvider } from "../image/image"
+
+/**
+    Provider that deals with getting and setting annotations
+ */
+@Injectable()
+export class AnnotationsProvider {
+    public annotations: AnnotationObject[] = [];
+
+    constructor(private imageProvider: ImageProvider) {
+    }
+
+    public initAnnotations(imageSrc: string) {
+        if (! this.annotations[imageSrc]) {
+            this.annotations[imageSrc] = {
+                src: imageSrc,
+                lines: [],
+                boxes: [],
+                polygons: []
+            } as AnnotationObject;
+        }
+    }
+
+    getBoxes() {
+        let currentImage = this.imageProvider.currentImage;
+        if (currentImage) {
+            return this.annotations[currentImage.src].boxes
+        } else {
+            return [];
+        }
+    }
+
+    addBox(box) {
+        console.log("addBox():");
+        let currentImage = this.imageProvider.currentImage;
+        if (currentImage ) {
+            this.annotations[currentImage.src].boxes.push(box);
+        }
+    }
+
+    getLines() {
+
+        let currentImage = this.imageProvider.currentImage;
+        if (currentImage) {
+            return this.annotations[currentImage.src].lines
+        } else {
+            return [];
+        }
+    }
+
+    addLine(line: Line) {
+
+        let currentImage = this.imageProvider.currentImage;
+        if( currentImage ) {
+            this.annotations[currentImage.src].lines.push(line);
+        }
+
+    }
+
+}
+
+export class Line {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number
+}
+
+export class Box {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number
+}
