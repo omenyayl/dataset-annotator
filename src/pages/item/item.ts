f904f586bdaf0bd3151d9ee6d829c0ef5dcd3bfa
Oleg Menyaylenko <omenyayl@masonlive.gmu.edu>
2018-07-18 19:52:13 -0400

    removed unused imports

--- src/pages/item/item.ts
+++ src/pages/item/item.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+import {IonicPage, NavParams} from 'ionic-angular';
 import {_DetailPage} from '../_DetailPage';
 import {FileProvider} from "../../providers/file/file";
 import * as path from 'path';
